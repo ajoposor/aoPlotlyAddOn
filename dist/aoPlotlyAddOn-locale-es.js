52a04963fd65cf6358c3c883249145e14b8dd991
Antonio O <ajoposor@gmail.com>
2018-01-30 14:41:05 -0300

    Create aoPlotlyAddOn-locale-es.js

---
+++ dist/aoPlotlyAddOn-locale-es.js
@@ -0,0 +1,34 @@
+aoPlotlyAddOn.register({
+	moduleType: "locale",
+	name: "es",
+	dictionary: {
+		"close": "cierre",
+		"avg.": "prom.",
+		"chg.": "var.",
+		"% chg.", "%var.",
+		"% chg.^2": "%var.^2",
+		"cum.": "acum.",
+		"log": "log",
+		"linear": "lineal",
+		"real": "real",
+		"nominal": "nominal",
+		"compared": "comparados",
+		"uncompared": "no comparados",
+		"daily": "diario",
+		"weekly": "semanal",
+		"monthly": "mensual",
+		"quarterly": "trimestral",
+		"semiannual": "semestral",
+		"annual": "anual",
+		"1m": "1m",
+		"6m": "6m",
+		"YTD": "YTD",
+		"5y": "5a,
+		"10y": "10a",
+		"all": "todo"	
+		
+	},
+	format: {
+
+	}
+});
